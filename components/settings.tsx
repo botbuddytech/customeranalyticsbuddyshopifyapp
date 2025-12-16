2094020b98f213480a5b1997dc9ba258def03d5e
Biki Mandal <bikimandal1832000@gmail.com>
2025-12-16 16:39:31 +0530

    tuesday-4:39-push

---
+++ components/settings.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings Component</h1>
+      <p>This is a placeholder for the Settings component.</p>
+    </div>
+  );
+};
+
+export default Settings;
